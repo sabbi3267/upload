44fd66b64a8e37b2c5dd0df8ecae867d2902e090
Noor <nalam@svsu.edu>
2019-04-03 15:02:19 -0400

    Update customers.php

--- customers.php
+++ customers.php
@@ -2,46 +2,33 @@
 
 session_start();
 
-// include the class that handles database connections
+
 require "database.php";
 
-// include the class containing functions/methods for "customer" table
-// Note: this application uses "customer" table, not "cusotmers" table
+
 require "customer.class.php";
 $cust = new Customer();
 
-// set active record field values, if any 
-// (field values not set for display_list and display_create_form)
-if(isset($_GET["id"]))          $id = $_GET["id"]; 
-if(isset($_POST["name"]))       $cust->name = htmlspecialchars($_POST["name"]);
-if(isset($_POST["email"]))      $cust->email = htmlspecialchars($_POST["email"]);
-if(isset($_POST["mobile"]))     $cust->mobile = htmlspecialchars($_POST["mobile"]);
-if(isset($_POST["username"]))   $cust->username = htmlspecialchars($_POST["username"]);
-if(isset($_POST["password"]))   $cust->password = htmlspecialchars($_POST["password"]);
 
 // "fun" is short for "function" to be invoked 
 if(isset($_GET["fun"])) {
 	$fun = $_GET["fun"];
 	if (!isset($_SESSION["user_id"])) { // if not logged in
-		if (!($fun == "display_create_form" || $fun == "check_login" || $fun == "insert_db_record" || $fun == "verify_email" || $fun == "send_email")) { // if not going to allowed page, then go to login page
+		if (!($fun == "display_create_form" ||  $fun == "insert_db_record" )) { 
 			$fun="display_login_view";
 		}
 	}
 }
-else $fun = "display_login_view"; 
+else $fun = "display_login"; 
 
 switch ($fun) {
 	case "list_pics":			$cust->list_pics();
-		break;
-	case "send_email":			$cust->send_email();
-		break;
-	case "verify_email":		$cust->verify_email();
 		break;
 	case "logout":				$cust->logout();
 		break;
 	case "check_login":			$cust->check_login();
 		break;
-	case "display_login_view":  $cust->login_view();
+	case "display_login":  		$cust->login_view();
 		break;
     case "display_list":        $cust->list_records();
         break;
