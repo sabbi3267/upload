18d4ace1cff2029b82fdaccfc4559af54ee4d217
Noor <nalam@svsu.edu>
2019-04-22 22:14:56 -0400

    Update customers.php

--- customers.php
+++ customers.php
@@ -10,10 +10,10 @@ require "customer.class.php";
 $cust = new Customer();
 
 
-// "fun" is short for "function" to be invoked 
+
 if(isset($_GET["fun"])) {
 	$fun = $_GET["fun"];
-	if (!isset($_SESSION["user_id"])) { // if not logged in
+	if (!isset($_SESSION["user_id"])) { 
 		if (!($fun == "display_create_form" ||  $fun == "insert_db_record" )) { 
 			$fun="display_login_view";
 		}
@@ -22,32 +22,40 @@ if(isset($_GET["fun"])) {
 else $fun = "display_login"; 
 
 switch ($fun) {
+	case "logout":				$cust->logout();
+		break;
+	case "check_login":			$cust->check_login();
+		break;
+	case "display_login_view":  		$cust->login_view();
+		break;
+    	case "display_list":        		$cust->list_records();
+		break;
 	case "list_pics":			$cust->list_pics();
 		break;
 	case "logout":				$cust->logout();
 		break;
 	case "check_login":			$cust->check_login();
 		break;
-	case "display_login":  		$cust->login_view();
+	case "display_login":  			$cust->login_view();
 		break;
-    case "display_list":        $cust->list_records();
-        break;
-    case "display_create_form": $cust->create_record();
-        break;
-    case "display_read_form":   $cust->read_record($id); 
-        break;
-    case "display_update_form": $cust->update_record($id);
-        break;
-    case "display_delete_form": $cust->delete_record($id); 
-        break;
-    case "insert_db_record":    $cust->insert_db_record();
-        break;
-    case "update_db_record":    $cust->update_db_record($id);
-        break;
-    case "delete_db_record":    $cust->delete_db_record($id);
-        break;
-    default: 
-        echo "Error: Invalid function call (customer.php)";
+    	case "display_list":        		$cust->list_records();
+        	break;
+    	case "display_create_form": 		$cust->create_record();
+        	break;
+    	case "display_read_form":   		$cust->read_record($id); 
+       		 break;
+    	case "display_update_form": 		$cust->update_record($id);
+        	break;
+    	case "display_delete_form": 		$cust->delete_record($id); 
+        	break;
+    	case "insert_db_record":    		$cust->insert_db_record();
+        	break;
+    	case "update_db_record":    		$cust->update_db_record($id);
+        	break;
+    	case "delete_db_record":    		$cust->delete_db_record($id);
+       		 break;
+    	default: 
+        echo " Invalid function call ";
         exit();
         break;
 }
